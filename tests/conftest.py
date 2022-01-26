e8f7749f31fc1c40beaf4b7fdca904853cfe9c8d
kevinheavey <kevinheavey123@gmail.com>
2022-01-26 16:02:26 +0000

    unused import

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,7 @@
 import asyncio
 from pytest import fixture
 from pytest_asyncio import fixture as async_fixture
-from anchorpy import Provider, WorkspaceType, workspace_fixture, Program
+from anchorpy import Provider, WorkspaceType, workspace_fixture
 from solana.keypair import Keypair
 from solana.transaction import Transaction
 from solana.system_program import create_account, CreateAccountParams
