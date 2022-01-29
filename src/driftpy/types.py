615aed4c086e576f6501ef194275ec22ab03b91b
kevinheavey <kevinheavey123@gmail.com>
2022-01-29 17:22:08 +0000

    update type

--- src/driftpy/types.py
+++ src/driftpy/types.py
@@ -103,46 +103,6 @@ class StateAccount:
     padding7: int
 
 
-# @dataclass
-# class StateAccount:
-#     admin: PublicKey
-#     funding_paused: bool
-#     exchange_paused: bool
-#     admin_controls_prices: bool
-#     collateral_mint: PublicKey
-#     collateral_vault: PublicKey
-#     collateral_vault_authority: PublicKey
-#     collateral_vault_nonce: int
-#     insurance_vault: PublicKey
-#     insurance_vault_authority: PublicKey
-#     insurance_vault_nonce: int
-#     margin_ratio_initial: int
-#     margin_ratio_maintenance: int
-#     margin_ratio_partial: int
-#     markets: PublicKey
-#     curve_history: PublicKey
-#     deposit_history: PublicKey
-#     funding_rate_history: PublicKey
-#     funding_payment_history: PublicKey
-#     trade_history: PublicKey
-#     liquidation_history: PublicKey
-#     partial_liquidation_close_percentage_numerator: int
-#     partial_liquidation_close_percentage_denominator: int
-#     partial_liquidation_penalty_percentage_numerator: int
-#     partial_liquidation_penalty_percentage_denominator: int
-#     full_liquidation_penalty_percentage_numerator: int
-#     full_liquidation_penalty_percentage_denominator: int
-#     partial_liquidation_liquidator_share_denominator: int
-#     full_liquidation_liquidator_share_denominator: int
-#     fee_structure: FeeStructure
-#     total_fee: int
-#     total_fee_withdrawn: int
-#     whitelist_mint: PublicKey
-#     discount_mint: PublicKey
-#     oracle_guard_rails: OracleGuardRails
-#     max_deposit: int
-
-
 @_rust_enum
 class OracleSource:
     Pyth = constructor()
