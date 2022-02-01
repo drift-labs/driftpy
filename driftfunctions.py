49c6aba628a1b32d0a39d2f75b38a806ba6e3ac7
Slamwise <58749535+Slamwise@users.noreply.github.com>
2022-02-01 16:36:17 -0500

    Update driftfunctions.py

--- driftfunctions.py
+++ driftfunctions.py
@@ -216,6 +216,4 @@ async def main():
 
     print(calculateTradeSlippage('LONG', 5000, markets, 'SOL'))
 
-if sys.version_info[0] == 3 and sys.version_info[1] >= 8 and sys.platform.startswith('win'):
-    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 asyncio.run(main())
