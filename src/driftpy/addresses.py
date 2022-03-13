6e59f17b5a5e8c622c9cee208a2e70f93a8b508f
Brian Giarrocco <chrysanthemum@gmail.com>
2022-03-13 11:22:38 -0400

    Order inx (#8)
    * Adding place_order_ix
    
    * Adding additional order ix's
    
    Co-authored-by: iwillnotsaveyou <dontemailme@drift.trade>

--- src/driftpy/addresses.py
+++ src/driftpy/addresses.py
@@ -16,3 +16,9 @@ def get_user_orders_account_public_key_and_nonce(
     return PublicKey.find_program_address(
         [b"user_orders", bytes(user_account_public_key)], program_id
     )
+
+
+def get_order_state_account_public_key_and_nonce(
+    program_id: PublicKey
+) -> tuple[PublicKey, int]:
+    return PublicKey.find_program_address([b"order_state"], program_id)
