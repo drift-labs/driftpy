a23d646e0b27d1502a45240bd5adc27fd72ea342
0xNineteen <0x39015319@gmail.com>
2022-11-02 12:26:11 +0000

    update to newest program

--- src/driftpy/constants/config.py
+++ src/driftpy/constants/config.py
@@ -18,7 +18,7 @@ configs = {
     "devnet": Config(
         env='devnet',
         pyth_oracle_mapping_address=PublicKey('BmA9Z6FjioHJPpjT39QazZyhDRUdZy2ezwx4GiDdE2u2'),
-		clearing_house_program_id=PublicKey('jAEeKs9twxAJmXZHqS2p459xW7FMDjoyvuqthRo9qGS'),
+		clearing_house_program_id=PublicKey('4PzAUP84r19y2whicgTysCiqxw5aHabLHyDh3oJiAtqe'),
 		usdc_mint_address=PublicKey('8zGuJQqwhZafTah7Uc7Z4tXRnguqkn5KLFAP8oV6PHe2'),
 		markets=devnet_markets,
 		banks=devnet_banks,
