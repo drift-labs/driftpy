80a78a464d301247f5311ca1ffe037b256085afd
sina <20732540+SinaKhalili@users.noreply.github.com>
2024-12-20 15:30:18 -0800

    Oracle: formatting

--- tests/integration/oracle.py
+++ tests/integration/oracle.py
@@ -1,30 +1,28 @@
 import asyncio
 from math import sqrt
 
+from anchorpy import Program, Provider, WorkspaceType, workspace_fixture
 from pytest import fixture, mark
 from pytest_asyncio import fixture as async_fixture
 from solders.keypair import Keypair
 from solders.pubkey import Pubkey
-from anchorpy import Program, Provider, WorkspaceType, workspace_fixture
+
 from driftpy.account_subscription_config import AccountSubscriptionConfig
 from driftpy.accounts.bulk_account_loader import BulkAccountLoader
 from driftpy.accounts.get_accounts import (
     get_perp_market_account,
     get_spot_market_account,
 )
-
-from driftpy.constants.numeric_constants import *
 from driftpy.admin import Admin
+from driftpy.constants.numeric_constants import *
 from driftpy.drift_client import DriftClient
 from driftpy.setup.helpers import (
-    _create_mint,
     _create_and_mint_user_usdc,
+    _create_mint,
+    initialize_sol_spot_market,
     mock_oracle,
-    set_price_feed,
 )
 from driftpy.types import *
-from driftpy.setup.helpers import initialize_sol_spot_market
-
 
 workspace = workspace_fixture("protocol-v2", build_cmd="anchor build", scope="session")
 
