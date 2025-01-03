05ff421c6c6dddc51ab57ee090ff09c1e0b8305b
sina <20732540+SinaKhalili@users.noreply.github.com>
2025-01-03 15:03:28 -0800

    Update auction subscriber to use event subclass

--- src/driftpy/auction_subscriber/auction_subscriber.py
+++ src/driftpy/auction_subscriber/auction_subscriber.py
@@ -1,15 +1,27 @@
 import asyncio
 from typing import Optional
+
+from events.events import Events, _EventSlot
+from solders.pubkey import Pubkey
+
 from driftpy.accounts.types import DataAndSlot, WebsocketProgramAccountOptions
 from driftpy.accounts.ws.program_account_subscriber import (
     WebSocketProgramAccountSubscriber,
 )
 from driftpy.auction_subscriber.types import AuctionSubscriberConfig
-from events import Events as EventEmitter
-from driftpy.memcmp import get_user_filter, get_user_with_auction_filter
 from driftpy.decode.user import decode_user
-from solders.pubkey import Pubkey
+from driftpy.memcmp import get_user_filter, get_user_with_auction_filter
 from driftpy.types import UserAccount
+
+
+class AuctionEvents(Events):
+    """
+    Subclass to explicitly declare the on_account_update event
+    both in __events__ and as a typed attribute.
+    """
+
+    __events__ = ("on_account_update",)
+    on_account_update: _EventSlot
 
 
 class AuctionSubscriber:
@@ -22,12 +34,13 @@ class AuctionSubscriber:
         )
         self.resub_timeout_ms = config.resub_timeout_ms
         self.subscriber: Optional[WebSocketProgramAccountSubscriber] = None
-        self.event_emitter = EventEmitter(("on_account_update"))
-        self.event_emitter.on("on_account_update")
+        self.event_emitter = AuctionEvents()
 
     async def on_update(self, account_pubkey: str, data: DataAndSlot[UserAccount]):
         self.event_emitter.on_account_update(
-            data.data, Pubkey.from_string(account_pubkey), data.slot
+            data.data,
+            Pubkey.from_string(account_pubkey),
+            data.slot,  # type: ignore
         )
 
     async def subscribe(self):
