88d919c68fcad1d41cd55ccf1ebaa228f1af51ac
sina <20732540+SinaKhalili@users.noreply.github.com>
2025-02-07 14:25:33 -0800

    Update helper for posted slot tail

--- tests/math/helpers.py
+++ tests/math/helpers.py
@@ -2,25 +2,27 @@ import asyncio
 from copy import deepcopy
 
 from anchorpy import Wallet
-from driftpy.constants.numeric_constants import PRICE_PRECISION
-from driftpy.drift_client import DriftClient
-from driftpy.drift_user import DriftUser
-from driftpy.types import MarginMode
-from driftpy.types import MarketType
-from driftpy.types import OraclePriceData
-from driftpy.types import Order
-from driftpy.types import OrderStatus
-from driftpy.types import OrderTriggerCondition
-from driftpy.types import OrderType
-from driftpy.types import PerpPosition
-from driftpy.types import PositionDirection
-from driftpy.types import SpotBalanceType
-from driftpy.types import SpotPosition
-from driftpy.types import UserAccount
 from solana.rpc.async_api import AsyncClient
 from solders.keypair import Keypair  # type: ignore
 from solders.pubkey import Pubkey  # type: ignore
 
+from driftpy.constants.numeric_constants import PRICE_PRECISION
+from driftpy.drift_client import DriftClient
+from driftpy.drift_user import DriftUser
+from driftpy.types import (
+    MarginMode,
+    MarketType,
+    OraclePriceData,
+    Order,
+    OrderStatus,
+    OrderTriggerCondition,
+    OrderType,
+    PerpPosition,
+    PositionDirection,
+    SpotBalanceType,
+    SpotPosition,
+    UserAccount,
+)
 
 mock_perp_position = PerpPosition(
     base_asset_amount=0,
@@ -75,7 +77,8 @@ mock_order = Order(
     auction_start_price=0,
     auction_end_price=0,
     max_ts=0,
-    padding=[0, 0, 0],
+    posted_slot_tail=0,
+    padding=[0, 0],
 )
 
 mock_user_account = UserAccount(
