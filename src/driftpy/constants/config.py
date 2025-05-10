626b008ce2373acdeb176ad921095b458d5374eb
Srijan Jaiswal <srijan.jaiswal@auros.global>
2025-05-10 19:30:38 +0530

    websocket logprovider endpoint fix and increase timeout for getProgramAccounts

--- src/driftpy/constants/config.py
+++ src/driftpy/constants/config.py
@@ -178,7 +178,7 @@ async def find_all_market_and_oracles(
         json=perp_request,
         headers={"content-encoding": "gzip"},
     )
-    resp = await asyncio.wait_for(post, timeout=10)
+    resp = await asyncio.wait_for(post, timeout=30)
     parsed_resp = jsonrpcclient.parse(resp.json())
 
     if isinstance(parsed_resp, jsonrpcclient.Error):
@@ -210,7 +210,7 @@ async def find_all_market_and_oracles(
         json=spot_request,
         headers={"content-encoding": "gzip"},
     )
-    resp = await asyncio.wait_for(post, timeout=10)
+    resp = await asyncio.wait_for(post, timeout=30)
     parsed_resp = jsonrpcclient.parse(resp.json())
 
     if isinstance(parsed_resp, jsonrpcclient.Error):
